9ce22875c3661b8862f1b5a89f150c382789ed15
Romit Samanta <22936532+dr-romster@users.noreply.github.com>
2024-01-31 11:30:49 +0000

    Create transcript_to_symbol.R
    Ensembl trancripts to gene symbols

---
+++ transcript_to_symbol.R
@@ -0,0 +1,37 @@
+# Bulk RNA-Sequencing
+# There are several ways of identifying a gene symbol from Ensembl transcript codes (ENST...) in R
+# Unfortunately the Ensembl servers have become unreliable recently which precludes the `biomaRt` 
+# methods. 
+
+# This is a simple workaround that does not rely on connection to an remote server
+# beyond the package installation process. 
+
+library(dplyr)
+BiocManager::install("EnsDb.Hsapiens.v86")
+BiocManager::install("AnnotationDbi")
+
+# transcripts from count data frame
+transcript_list <- row.names(counts_df)
+
+# Transcripts may have a suffix depending on the aligment tools used to 
+# identify them. 
+# Remove these
+
+transcript_list_clean <- stringr::str_replace(transcript_list,
+                        pattern = ".[0-9]+$",
+                        replacement = "")
+
+library(EnsDb.Hsapiens.v86)
+# this is a database object for humans that can be accessed using 
+# AnnotationDbi functions 
+ 
+db <- EnsDb.Hsapiens.v86
+
+annotated_transcript_list <- AnnotationDbi::select(db, 
+                                                   keys = transcript_list_clean,
+                                                   columns = c("ENTREZID", "SYMBOL", "GENEID", "GENENAME"),
+                                                   keytype = "TXID") %>% 
+                            tibble::as_tibble()
+
+
+
